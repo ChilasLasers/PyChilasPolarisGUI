b9b25fb361449c7478173e687405426847d1f803
Sebastian <s.dorobantu@student.utwente.nl>
2026-05-13 14:38:34 +0200

    fix icon issue

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-#****************************************************************************
-#This file is part of Chilas Polaris.
+# ****************************************************************************
+# This file is part of Chilas Polaris.
 #
-#Chilas Polaris is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# Chilas Polaris is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#Chilas Polaris is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#GNU General Public License for more details.
+# Chilas Polaris is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License
-#along with this program. If not, see <https://www.gnu.org/licenses/>.
-#****************************************************************************
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# ****************************************************************************
 
 import sys
 import os
@@ -27,7 +27,8 @@ import pyLaser
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     app.setOrganizationName("Chilas Lasers B.V.")
-    app.setWindowIcon(QIcon("icon.ico"))
+    icon_path = os.path.join(os.path.dirname(__file__), "icon.ico")
+    app.setWindowIcon(QIcon(icon_path))
 
     engine = QQmlApplicationEngine()
 
